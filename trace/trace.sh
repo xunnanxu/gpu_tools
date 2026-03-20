95f13b0299941090693382e1540a630a6bd574e3
Shawn Xu <xunnan.xu@gmail.com>
2026-03-20 10:57:35 -0700

    release

--- trace/trace.sh
+++ trace/trace.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 cargo build --release --manifest-path "$SCRIPT_DIR/Cargo.toml" --quiet
-exec "$SCRIPT_DIR/target/debug/trace" "$@"
+exec "$SCRIPT_DIR/target/release/trace" "$@"
